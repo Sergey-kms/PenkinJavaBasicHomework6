57a335f6f03407cd1da7ebad983c10d473c38745
penkinsergey <penkinsergeykms@gmail.com>
2023-11-22 17:56:20 +0800

    Commit1 branch vetka1

--- src/main/java/ru/java/basic/penkin/hw6/Cat.java
+++ src/main/java/ru/java/basic/penkin/hw6/Cat.java
@@ -1,2 +1,23 @@
-package ru.java.basic.penkin.hw6;public class Cat {
+package ru.java.basic.penkin.hw6;
+
+public class Cat {          // В гитхаб master
+    private String name;
+    private int appetite;
+    private boolean satiety;
+
+    public Cat(String name, int appetite) {
+        this.name = name;
+        this.appetite = appetite;
+        this.satiety = false;
+    }
+
+    public void eat(Plate plate) {
+        if (plate.reduceFood(appetite)) {
+            satiety = true;
+        }
+    }
+
+    public void printSatiety() {
+        System.out.println(name + (satiety ? " сытый" : " голоден"));
+    }
 }
